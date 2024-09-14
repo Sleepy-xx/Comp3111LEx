845efd71139084ee15a912071baa94be56a5671c
ZGX <qqzgxlxuan@gmail.com>
2024-09-14 16:29:33 +0800

    Delete 3rd print line

--- src/main/java/Lab1/mainApp1.java
+++ src/main/java/Lab1/mainApp1.java
@@ -9,7 +9,6 @@ public class mainApp1 {
         int n = 11;
         System.out.println("Welcome to Scientific Calculator");
         System.out.println("Program ..Starting...");
-        System.out.println("1 + 1 ="+2);
         System.out.println(b+" to power "+n+" = "+ myLibrary.Power(b,n));
         System.out.println(n+"! = " + myLibrary.factorial(n));
         System.out.println("Program ..Ended …");
